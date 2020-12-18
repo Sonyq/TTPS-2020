38474b96d17ffa38f113009e30e27253420305fa
Sonyq <cuenta.40@hotmail.com>
2020-12-18 11:38:43 -0300

    update scrip db

--- Documentacion/segco.sql
+++ Documentacion/segco.sql
@@ -477,3 +477,9 @@ VALUES (11, 11, 31, '2020-11-08 01:42:20', NULL);
 
 INSERT INTO internacion_cama (id, internacion_id, cama_id, fecha_desde, fecha_hasta)
 VALUES (12, 12, 32, '2020-11-07 21:49:42', NULL);
+
+INSERT INTO regla (id,evento,expresion,accion) VALUES
+	 (1,'NUEVA EVOLUCION','evolucion.getSomnolencia()','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
+	 (2,'NUEVA EVOLUCION','evolucion.getMecanicaVentilatoria() matches "regular"  or  evolucion.getMecanicaVentilatoria() matches "mala"','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
+	 (3,'NUEVA EVOLUCION','evolucion.getInternacion().getFechaInicioSintomas().diff(evolucion.getFechaCarga()) .days() >= 10','aviso.alertar(paciente.getUsers(),"Evaluar alta",evento)'),
+	 (4,'NUEVA EVOLUCION','evolucion.getFrecuenciaRespiratoria() > 30','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)');
