f9a228d8941f3e141ffd32769cbf842cf9373e7a
Sonyq <cuenta.40@hotmail.com>
2020-12-22 21:03:24 -0300

    Update db script

--- Documentacion/segco.sql
+++ Documentacion/segco.sql
@@ -471,9 +471,9 @@ ALTER SEQUENCE internacion_cama_id_seq RESTART WITH 13;
 
 INSERT INTO regla (id,evento,expresion,accion) VALUES
 	 (1,'NUEVA EVOLUCION','evolucion.getSomnolencia()','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
-	 (2,'NUEVA EVOLUCION','evolucion.getMecanicaVentilatoria() matches "regular"  or  evolucion.getMecanicaVentilatoria() matches "mala"','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
-	 (3,'NUEVA EVOLUCION','evolucion.getInternacion().getFechaInicioSintomas().diff(evolucion.getFechaCarga()) .days() >= 10','aviso.alertar(paciente.getUsers(),"Evaluar alta",evento)'),
-	 (4,'NUEVA EVOLUCION','evolucion.getFrecuenciaRespiratoria() > 30','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)');
+	 (4,'NUEVA EVOLUCION','evolucion.getFrecuenciaRespiratoria() > 30','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
+	 (2,'NUEVA EVOLUCION','evolucion.getMecanicaVentilatoria() matches "/regular/"  or  evolucion.getMecanicaVentilatoria() matches "/mala/"','aviso.alertar(paciente.getUsers(),"Evaluar pase a UTI",evento)'),
+	 (3,'NUEVA EVOLUCION','evolucion.getInternacion().getFechaInicioSintomas().diff(evolucion.getFechaCarga()) .days >= 10','aviso.alertar(paciente.getUsers(),"Evaluar alta",evento)');
 
 ALTER SEQUENCE regla_id_seq RESTART WITH 5;
 
